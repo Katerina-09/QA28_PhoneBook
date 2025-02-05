a30d33f07d74427ba38b0f6af62485cd07e4ba25
Katerina-09 <firiall68@gmail.com>
2025-02-05 21:02:42 +0200

    New05

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,9 +1,17 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
+    @BeforeMethod
+    public void preCondition(){
+        //if SingOut present ===>logout
+        if(app.getHelperUser().isLogged())
+            app.getHelperUser().logout();
+
+    }
     @Test
     public void loginSuccess(){
 
